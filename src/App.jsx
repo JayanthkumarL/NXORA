d1dbb0f6fa0798caf0e078ae1c3474b276071a13
Jayanth <jjay07572@gmail.com>
2026-05-11 12:20:22 +0530

    feat: add Pricing component and integrate into App layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from './components/Hero';
 import Services from './components/Services';
 import Projects from './components/Projects';
 import About from './components/About';
-import Pricing from './components/Pricing';
+// import Pricing from './components/Pricing';
 // import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -24,7 +24,7 @@ function App() {
         <About />
         <Services />
         <Projects />
-        <Pricing />
+        {/* <Pricing /> */}
         
         {/* <Testimonials /> */}
         <Contact />
